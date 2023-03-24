0686ef14880ff3cd60c7c91c4d2bb8632c9b0ad5
azf20 <azfuller20@gmail.com>
2023-03-24 00:41:38 +0000

    import useCallback

--- packages/react-app/src/CreateInk.js
+++ packages/react-app/src/CreateInk.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./App.css";
