7cb076ae3dddf6d053e8f5c8a4168db5287b95e3
Sherkhan Azimov <sherkhanazim@gmail.com>
2025-03-25 17:27:08 +0100

    refactor: update layout and styling for ZoraPosts component

--- packages/nextjs/app/_components/ZoraPosts.tsx
+++ packages/nextjs/app/_components/ZoraPosts.tsx
@@ -17,42 +17,34 @@ const ZoraPosts: React.FC<ZoraPostsProps> = ({ isLoading, posts }) => {
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="flex items-center justify-center">
-        <ul className="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-          {posts.map((post, index) => {
-            return (
-              <li
-                key={`${post.name}-${index}`}
-                className="border-2 border-gray-200 rounded-lg p-2 transition-transform hover:scale-105  max-w-[150px]"
-              >
-                <Link
-                  href={`https://zora.co/coin/base:${post.contractAddress}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="block w-full"
-                >
-                  <div className="aspect-square w-full mx-auto">
-                    <LazyImage
-                      uri={post?.uri}
-                      alt={post?.name as string}
-                      width={0}
-                      height={0}
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <h3 className="my-2 text-sm md:text-md lg:text-md xl:text-md font-bold truncate w-full text-center">
-                      {post.name}
-                    </h3>
-                  </div>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+    <ul className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-6 max-w-4xl mx-auto px-4">
+      {posts.map((post, index) => (
+        <li
+          key={`${post.name}-${index}`}
+          className="border-2 border-gray-200 rounded-lg p-2 transition-transform hover:scale-105 w-full"
+        >
+          <Link
+            href={`https://zora.co/coin/base:${post.contractAddress}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block w-full"
+          >
+            <div className="aspect-square w-full">
+              <LazyImage
+                uri={post?.uri}
+                alt={post?.name as string}
+                width={0}
+                height={0}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <h3 className="my-2 text-sm md:text-md lg:text-md xl:text-md font-bold truncate w-full text-center">
+              {post.name}
+            </h3>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
