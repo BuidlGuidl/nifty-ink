4e4527eea0e32a446c296b31d75e7635504c5972
Sherkhan Azimov <sherkhanazim@gmail.com>
2025-04-07 20:56:05 +0200

    fix: remove close on click outside of box

--- packages/nextjs/app/create/_components/publish/PublishModal.tsx
+++ packages/nextjs/app/create/_components/publish/PublishModal.tsx
@@ -14,7 +14,7 @@ export const PublishModal = ({ connectedAddress, modalId, chain, drawingCanvas }
     <>
       <div>
         <input type="checkbox" id={`${modalId}`} className="modal-toggle" />
-        <label htmlFor={`${modalId}`} className="modal cursor-pointer">
+        <label className="modal">
           <label className="modal-box relative">
             {/* dummy input to capture event onclick on modal box */}
             <input className="h-0 w-0 absolute top-0 left-0" />
